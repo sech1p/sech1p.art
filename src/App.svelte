f56f4b381c0d4826538b6d965f6598ed3414f965
sech1p <sech1p@disroot.org>
2024-09-25 14:40:20 +0200

    🔧 Change placeholder links in header to real one

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,10 @@
   <header class="header flex flex-1 bg-black/80 fixed top-0 left-0 backdrop-filter backdrop-blur-md items-center justify-between w-full z-100">
     <a href="#" class="logo text-white"><span>sec</span>h1p</a>
     <nav class="navbar text-xl text-white">
-      <a href="#" class="active nav-link">Portfolio</a>
+      <a href="#" class="active nav-link">Home</a>
+      <a href="#" class="nav-link">My interests</a>
       <a href="#" class="nav-link">Portfolio</a>
-      <a href="#" class="nav-link">Portfolio</a>
-      <a href="#" class="nav-link">Portfolio</a>
-      <a href="#" class="nav-link">Portfolio</a>
+      <a href="#" class="nav-link">Donations</a>
     </nav>
     <a href="#" class="contact bg-white text-black border-2 rounded-3xl text-base hover:text-white">Contact Me</a>
   </header>
