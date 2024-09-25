03f04804250037e0debbac7eaf26e423e36e4b7f
sech1p <sech1p@disroot.org>
2024-09-25 13:17:43 +0200

    ➕ Add boxicons icons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import App from "./App.svelte";
 import "./style.css";
 import "@fortawesome/fontawesome-free/css/all.css";
+import "boxicons";
 
 const app = new App({
   target: document.getElementById("app"),
