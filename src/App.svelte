d4f3cd07cdf29d0dbd7ce97d95c9af46cfa01ff8
sech1p <sech1p@disroot.org>
2024-09-25 14:45:27 +0200

    ➕ Add svelte-routing into page

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,25 @@
-<main>
+<script>
+  import {
+    Router,
+    Route,
+    Link,
+  } from "svelte-routing";
+  import Home from "./pages/Home.svelte";
+</script>
+
+<Router>
   <header class="header flex flex-1 bg-black/80 fixed top-0 left-0 backdrop-filter backdrop-blur-md items-center justify-between w-full z-100">
-    <a href="#" class="logo text-white"><span>sec</span>h1p</a>
+    <Link to="/" class="logo text-white"><span>sec</span>h1p</Link>
     <nav class="navbar text-xl text-white">
-      <a href="#" class="active nav-link">Home</a>
+      <Link to="/" class="active nav-link">Home</Link>
       <a href="#" class="nav-link">My interests</a>
       <a href="#" class="nav-link">Portfolio</a>
       <a href="#" class="nav-link">Donations</a>
     </nav>
     <a href="#" class="contact bg-white text-black border-2 rounded-3xl text-base hover:text-white">Contact Me</a>
   </header>
-  <section class="home flex w-full min-h-screen bg-white items-center gap-28">
-    <div class="home-content">
-      <h3>Hi!</h3>
-      <h1>I'm <span>Eliza<br></span>a Fullstack Developer</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <div class="btns flex gap-8">
-        <button class="btn1 bg-black text-white rounded-2xl text-lg tracking-widget hover:bg-white hover:text-black">Hire me!</button>
-        <button class="btn2 text-white rounded-2xl text-lg tracking-widget hover:bg-white hover:text-black">About me</button>
-      </div>
-    </div>
-  </section>
-</main>
+
+  <main>
+    <Route path="/" component={Home} />
+  </main>
+</Router>
