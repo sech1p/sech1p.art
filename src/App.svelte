ae8db875fcf2468e797fe0fe135dc13be2a8d5bb
sech1p <sech1p@disroot.org>
2024-09-25 14:21:40 +0200

    ➕ Add homepage content

--- src/App.svelte
+++ src/App.svelte
@@ -10,4 +10,17 @@
     </nav>
     <a href="#" class="contact bg-white text-black border-2 rounded-3xl text-base hover:text-white">Contact Me</a>
   </header>
+  <section class="home flex w-full min-h-screen bg-white items-center gap-28">
+    <div class="home-content">
+      <h3>Hi!</h3>
+      <h1>I'm <span>Eliza<br></span>a Fullstack Developer</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <div class="btns flex gap-8">
+        <button class="btn1 bg-black text-white rounded-2xl text-lg tracking-widget hover:bg-white hover:text-black">Hire me!</button>
+        <button class="btn2 text-white rounded-2xl text-lg tracking-widget hover:bg-white hover:text-black">About me</button>
+      </div>
+    </div>
+  </section>
 </main>
