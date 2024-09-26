105fa248f198a63e96afd1da6ce1a00be7e73af0
sech1p <sech1p@disroot.org>
2024-09-26 14:13:41 +0200

    ➕ Add redirect to LinkedIn in Hire me button in home component

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -11,7 +11,12 @@
       I'm a passionate and dedicated software engineer. I specialize in building scalable and maintainable applications using modern technologies like TypeScript, WebSockets, and Svelte. I'm currently looking for opportunities to work on cutting-edge projects and contribute to the tech community.
     </p>
     <div class="btns flex gap-8">
-      <button class="btn1 bg-black text-white rounded-2xl text-lg tracking-widget hover:bg-white hover:text-black">Hire me!</button>
+      <button
+        on:click="{() => window.location.href = 'https://www.linkedin.com/in/eliza-semeniuk-18405628b/'}"
+        class="btn1 bg-black text-white rounded-2xl text-lg tracking-widget hover:bg-white hover:text-black"
+      >
+        Hire me!
+      </button>
       <button
         on:click="{() => document.getElementById('about').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })}"
         class="btn2 text-white rounded-2xl text-lg tracking-widget hover:bg-white hover:text-black"
