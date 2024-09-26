b4a2a66986567c6f13097dd9688c853ce8bdb8a1
sech1p <sech1p@disroot.org>
2024-09-26 07:20:04 +0200

    🌸 Update privacy policy

--- src/pages/PrivacyPolicy.svelte
+++ src/pages/PrivacyPolicy.svelte
@@ -1,11 +1,94 @@
 <section class="privacy-policy flex w-full min-h-screen bg-white items-center gap-28">
   <div class="privacy-policy-content">
-    <h2 class="text-5xl">Privacy Policy</h2>
-    <p class="text-xl mt-10">
-      This website (<a class="link" href="https://sech1p.ovh">sech1p.ovh</a>), does not collect any information about you.
+    <h2 class="text-5xl mt-10">Privacy Policy</h2>
+    <h3 class="text-3xl mt-10">Your Privacy Matters</h3>
+    <p class="text-lg mt-5">
+      This Privacy Policy explains, how I collect, use, disclose, and safeguard your information when you visit my portfolio website. (the "Website") and use the contact form to submit inquiries. Please read this policy carefully to understand practices regarding your personal information and how it is used.
     </p>
-    <p class="text-xl mt-10">
-      I believe that users have the right to privacy and such personal portfolio website does not collect and does not intend to collect any information about you. Enjoy ^^
+    <p class="text-lg mt-2">
+      By using this website, you consent to the collection, use, disclosure, and safeguarding of your personal information in accordance with this Privacy Policy.
     </p>
+    <div class="mt-10">
+      <h3 class="text-3xl mt-10">Information Collection and Use</h3>
+      <p class="text-xl mt-5">
+        I collect and use the following types of personal information when you visit the Website:
+      </p>
+      <ul class="privacy-policy-collected-data">
+        <li class="text-lg font-bold hover:underline">Name</li>
+        <li class="text-lg font-bold hover:underline">Email address</li>
+      </ul>
+    </div>
+    <div class="mt-10">
+      <h3 class="text-3xl mt-10">How your information it used</h3>
+      <p class="text-xl mt-5">
+        I use the personal information you provide through the contact form to:
+      </p>
+      <ul class="privacy-policy-collected-data">
+        <li class="text-lg font-bold hover:underline">Respond to your inquiries and communicate with you</li>
+        <li class="text-lg font-bold hover:underline">Provide you with information or services that you request</li>
+        <li class="text-lg font-bold hover:underline">Improve the Website and user experience</li>
+        <li class="text-lg font-bold hover:underline">Address any concerns you may have</li>
+      </ul>
+      <p class="text-xl mt-5">
+        Your information will <b>not</b> be used for marketing purposes or shared with third parties. Never.
+      </p>
+    </div>
+    <div class="mt-10">
+      <h3 class="text-3xl mt-10">Data Security</h3>
+      <p class="text-xl mt-5">
+        I implement reasonable administrative, technical and physical security measures to protect your personal information. However, no method of transmission over the Internet (or method of electronic storage) is 100% secure. While I strive to protect your personal information, I cannot guarante its absolute security. Sorry.
+      </p>
+    </div>
+    <div class="mt-10">
+      <h3 class="text-3xl mt-10">Data Retention</h3>
+      <p class="text-xl mt-5">
+        I will retain your personal information for as long as necessary to fulfill the purposes outlined in this Privacy Policy for which it was collected or as required by applicable laws.
+      </p>
+    </div>
+    <div class="mt-10">
+      <h3 class="text-3xl mt-10">Your Rights</h3>
+      <p class="text-xl mt-5">
+        Depending on your location, you may have the following rights regarding your personal information:
+      </p>
+      <ul class="privacy-policy-collected-data">
+        <li class="text-lg font-bold hover:underline">Access</li>
+        <p class="text-lg">You can request access to your personal information.</p>
+        <li class="text-lg font-bold hover:underline">Correction</li>
+        <p class="text-lg">You can request that your personal information be corrected if it is inaccurate, incomplete, or outdated.</p>
+        <li class="text-lg font-bold hover:underline">Deletion</li>
+        <p class="text-lg">You can request that your personal information be deleted.</p>
+        <li class="text-lg font-bold hover:underline">Restriction of Use</li>
+        <p class="text-lg">You can request that your personal information be restricted from being used at any time.</p>
+      </ul>
+      <p class="text-xl mt-5">
+        To exercise any of these rights, please contact me at <span>sech1p at disroot dot org</span>.
+      </p>
+    </div>
+    <div class="mt-10">
+      <h3 class="text-3xl mt-10">Third-Party Links</h3>
+      <p class="text-xl mt-5">
+        This Website may contain links to other websites. I am have not control for the privacy policies or practices of these third-party websites. Please visit their respective websites to understand their privacy policies.
+      </p>
+    </div>
+    <div class="mt-10">
+      <h3 class="text-3xl mt-10">Changes to This Privacy Policy</h3>
+      <p class="text-xl mt-5">
+        I may update this Privacy Policy from time to time. The updated version will be posted on this page.
+      </p>
+    </div>
+    <div class="mt-10">
+      <h3 class="text-3xl mt-10">Contact Us</h3>
+      <p class="text-xl mt-5">
+        If you have any questions regarding this Privacy Policy or how I handle your personal information, please contact me at:
+      </p>
+      <ul class="privacy-policy-collected-data">
+        <li class="text-lg font-bold hover:underline">Eliza Emilia Semeniuk</li>
+        <li class="text-lg font-bold hover:underline">E-mail</li>
+        <p class="text-lg"><span>sech1p at disroot dot org</span></p>
+      </ul>
+      <p class="text-sm mt-10 mb-10">
+        <i>Last Updated: 2024, 26 September</i>
+      </p>
+    </div>
   </div>
 </section>
