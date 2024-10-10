08efb3cc4b8c8ac0e23e38b0f188068dd34ceca4
sech1p <sech1p@disroot.org>
2024-10-10 09:20:16 +0200

    🔃 Update interests in About scetion

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -6,6 +6,6 @@
       Hello! I'm Eliza Emilia Semeniuk (<i>sech1p</i>). I am 19 years old. I have been interested in technology and IT since I was a child.
       I am currently studying computer science at university. Privately I am interested in many things in the world around us.
     </p>
-    <p>Apart from programming, I am also interested in things like photography, music production, computer graphics, computer drawing, nature, art, anime, rave and video games.</p>
+    <p>Apart from programming, I am also interested in things like art, photography, music production, computer graphics/drawing, etc.</p>
   </div>
 </section>
